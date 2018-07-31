e31e6f1ecdd8d1ff82082138b66ff11305f54dce
Jason Huey <jason.huey1@gmail.com>
2018-07-31 12:01:56 -0700

    final

--- js/Main.js
+++ js/Main.js
@@ -1,91 +1,96 @@
 // product constructor 
-function Card (name, set, image, price) {
+function AddItem (name, image, set, price) {
+	//name
 	this.name  = name
-	this.set = set
+	//image
 	this.image = image
+	//set
+	this.set = set
+	//price
 	this.price = price
 }
 
 //create vars, using the format above
 
-
-var plains = new Card ("Plains", "HOU", "img/plains.jpg", "$3.00")
-var island = new Card ("Island", "HOU", "img/island.jpg", "$3.00")
-var swamp = new Card ("Swamp", "HOU", "img/swamp.jpg", "$3.00")
-var mountain = new Card ("Mountain", "HOU", "img/mountain.jpg", "$3.00")
-var forest = new Card ("Forest", "HOU", "img/forest.jpg", "$3.00")
+var plains = new AddItem ("Plains", "img/plains.jpg", "HOU", "$3.00")
+var island = new AddItem ("Island",  "img/island.jpg", "HOU", "$3.00")
+var swamp = new AddItem ("Swamp", "img/swamp.jpg", "HOU", "$3.00")
+var mountain = new AddItem ("Mountain","img/mountain.jpg",  "HOU", "$3.00")
+var forest = new AddItem ("Forest",  "img/forest.jpg", "HOU", "$3.00")
 
 
 // create an array to add products to
-var cardArray = new Array
+var itemArray = []
 
 // add products to array of products
-cardArray.push(plains, island, swamp, mountain, forest)
+itemArray.push(plains, island, swamp, mountain, forest)
 
-
+function displayItems(array) {
 // loop through products array
-for(var i = 0; i < cardArray.length; i++) {
-	// create new elements for each product
-	var newItem  = document.createElement("div")
-	var newDiv   = document.createElement("div")
-	var nameH1   = document.createElement("h1")
-	var setH4   = document.createElement("h4")
-	var priceH4  = document.createElement("h4")
-	var btn      = document.createElement("button")
-	var image    = document.createElement("img")
+for(var i = 0; i < array.length; i++) {
+
+	// create container div element
+	var newProductDiv  = document.createElement("div")
+	//add class name to container div
+	newProductDiv.className = "col-md-4 newDiv animated slideInLeft"
+
+	//create elements to put into the container div element
+	var newName   = document.createElement("h1")
+	var newImage    = document.createElement("img")
+	var newSet   = document.createElement("h4")
+	var newPrice  = document.createElement("h4")
+	var newButton      = document.createElement("button")
 
 	// create text for new elements
-	var tName   = document.createTextNode(cardArray[i].name)
-	var tSet   = document.createTextNode("Set: " + cardArray[i].set)
-	var tPrice  = document.createTextNode("Price: " + cardArray[i].price)
-	var tButton = document.createTextNode("Buy Now!")
+	var nameText   = document.createTextNode(array[i].name)
+	var setText   = document.createTextNode("Set: " + array[i].set)
+	var priceText  = document.createTextNode("Price: " + array[i].price)
+	var buttonText = document.createTextNode("Buy Now!")
 
 	// update source attribute
-	image.src = cardArray[i].image
+	newImage.src = array[i].image
+	newImage.className = "img-fluid"
 
 	// update class attributes
-	image.className = "img-responsive"
-	btn.className = "btn btn-primary btn-lg"
-	newItem.className = "col-sm-4"
-	newDiv.className = "cards cardName" + [i] + "thumbnail"
+	newButton.className = "btn btn-primary btn-lg"
 
 	
 	// add text to elements
-	nameH1.appendChild(tName)
-	setH4.appendChild(tSet)
-	priceH4.appendChild(tPrice)
-	btn.appendChild(tButton)
+	newName.appendChild(nameText)
+	newSet.appendChild(setText)
+	newPrice.appendChild(priceText)
+	newButton.appendChild(buttonText)
 
 	// add elements to new div
-	newDiv.appendChild(nameH1)
-	newDiv.appendChild(image)
-	newDiv.appendChild(setH4)
-	newDiv.appendChild(priceH4)
-	newDiv.appendChild(btn)
+	newProductDiv.appendChild(newName)
+	newProductDiv.appendChild(newImage)
+	newProductDiv.appendChild(newSet)
+	newProductDiv.appendChild(newPrice)
+	newProductDiv.appendChild(newButton)
 
-	// add new div to new item div
-	newItem.appendChild(newDiv)
+	// // add new div to new item div
+	// newItem.appendChild(newDiv)
 
 	// add new item to the element with id="lands"
-	document.getElementById("cards").appendChild(newItem)
-}
+	document.getElementById("cards").appendChild(newProductDiv)
+}}
+
+displayItems(itemArray);
 
 
-
-
-function newProduct() {
+function newItem() {
 	// Create a new empty array variable
 	var newItemArray = [];
 	// Grab all of the values from the form fields
 	var itemName = document.getElementById("itemName").value;
 	var itemImage = document.getElementById("itemImage").value;
-	var itemImageAvail = (itemImage !== '') ? itemImage : "img/"
+	var itemImageAvail = (itemImage !== '') ? itemImage : "img/back.jpg"
 	var itemSet = document.getElementById("itemSet").value;
 	var itemPrice = document.getElementById("itemPrice").value;
 
-	var newProduct = new Card(itemName, itemImage, itemSet, itemPrice);
+	var newItem = new AddItem(itemName, itemImageAvail, itemSet, itemPrice);
 
-	newItemArray.push(newProduct)
-	cardArray(newItemArray);
+	newItemArray.push(newItem)
+	displayItems(newItemArray);
 	}
 
